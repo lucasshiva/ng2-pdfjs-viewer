30a05d6af498192889bd6a37bed4f5d2fb3ec045
Hunter Horsman <hhorsman@square-9.com>
2024-02-16 15:38:21 -0500

    build: correctly link the two projects

--- lib/auto_deploy.ps1
+++
@@ -1,9 +0,0 @@
-copy .\pdfjs\web\viewer.mjs ..\SampleApp\src\assets\pdfjs\web -Force
-npm run build
-cd dist
-npm pack
-copy .\ng2-pdfjs-viewer-17.0.4.tgz ..\..\SampleApp\ -Force
-cd ..\..\SampleApp\
-npm uninstall ng2-pdfjs-viewer
-npm install .\ng2-pdfjs-viewer-17.0.4.tgz
-npm start
