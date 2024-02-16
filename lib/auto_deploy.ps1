80919992f3788a7e4e2da2d1d510cb3486afec32
Hunter Horsman <hhorsman@square-9.com>
2024-02-16 11:07:38 -0500

    build: add ng-packagr dev dependency

--- lib/auto_deploy.ps1
+++ lib/auto_deploy.ps1
@@ -1,9 +1,9 @@
-copy .\pdfjs\web\viewer.js ..\SampleApp\src\assets\pdfjs\web -Force
+copy .\pdfjs\web\viewer.mjs ..\SampleApp\src\assets\pdfjs\web -Force
 npm run build
 cd dist
 npm pack
-copy .\ng2-pdfjs-viewer-17.0.3.tgz ..\..\SampleApp\ -Force
+copy .\ng2-pdfjs-viewer-17.0.4.tgz ..\..\SampleApp\ -Force
 cd ..\..\SampleApp\
 npm uninstall ng2-pdfjs-viewer
-npm install .\ng2-pdfjs-viewer-17.0.3.tgz
+npm install .\ng2-pdfjs-viewer-17.0.4.tgz
 npm start
